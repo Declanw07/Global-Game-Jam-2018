7af609d8aac5d9d3d792d2a55a9ee8f5b54eb4a2
Declan Wylie <declanw07@hotmail.com>
2018-01-26 18:10:29 +0000

    Changes to Initial Project

---
+++ Game Jame 2018 Prototype/Assets/Scripts/HeatBehaviourScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HeatBehaviourScript : MonoBehaviour {
+
+    public uint heatValue;
+
+    public 
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void HeatUpdate(short heatChange)
+    {
+
+        heatValue += (uint)heatChange;
+         
+
+    }
+
+    void UpdateHeatUI()
+    {
+
+
+
+    }
+}
