736335b49c3c8f1cc713365ac7c948b3fb09d4ad
Declan Wylie <declanw07@hotmail.com>
2018-01-26 18:28:37 +0000

    Updated to include Art Folder
    Inclusion of Art folder

---
+++ Game Jame 2018 Prototype/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerScript : MonoBehaviour {
+
+    [Range(0, 100)]
+    public short heatValue;
+
+    public Image heatBar;
+
+
+    private Sprite heatBarSprite;
+
+	// Use this for initialization
+	void Start () {
+
+        heatBarSprite = heatBar.GetComponent<Sprite>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
